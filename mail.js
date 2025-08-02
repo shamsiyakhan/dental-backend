0c80c3ff585651be2a65acb627520ae2d0a9e2c9
Shayban Khan <shaybankhan12345@gmail.com>
2025-08-02 13:43:23 +0530

    api added

--- mail.js
+++ mail.js
@@ -1,36 +1,5 @@
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
-const sendGridApiKey = process.env.SENDGRID_API_KEY;
-sgMail.setApiKey(sendGridApiKey);
 
 
 
-
-function sendOtp(email, otp) {
-    const sendEmail = async () => {
-        const msg = {
-            to: email,
-            from: 'sehrozkhan2704@gmail.com', // Your verified SendGrid sender email
-            subject: 'subject',
-            text: otp,
-            html: otp,
-        };
-
-        try {
-            await sgMail.send(msg);
-            console.log('Email sent successfully');
-        } catch (error) {
-            console.error('Error sending email:', error);
-            if (error.response) {
-                console.error(error.response.body);
-            }
-        }
-    };
-
-    sendEmail();
-}
-
-
-sendOtp('fareehachoglay29@gmail.com', 'faree tu kamini hai ');
-
-exports.sendOtp = sendOtp;
